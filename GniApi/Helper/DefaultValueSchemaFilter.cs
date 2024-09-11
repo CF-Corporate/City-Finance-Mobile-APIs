e365326ed88db04b7f7286e6d5a8f7004bf8be3c
NijatD <nijat.soltanov@ferrumcapital.az>
2024-09-11 11:58:20 +0400

    add:SwaggerDefaultValue

---
+++ GniApi/Helper/DefaultValueSchemaFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+namespace GniApi.Helper
+{
+    public class DefaultValueSchemaFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (schema.Properties != null)
+            {
+                foreach (var property in schema.Properties.Values)
+                {
+                    // If the property has a default value, set it in the Swagger documentation
+                    if (property.Default != null)
+                    {
+                        property.Example = property.Default;
+                    }
+                }
+            }
+        }
+    }
+}
